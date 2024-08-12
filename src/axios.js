5c0459a5c2e60c656741bf1a6e10020cc3ab603b
Islahihyaa <islah.ihya@gmail.com>
2024-08-12 09:36:41 +0700

    Change timeout

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000/api/v1',
-    timeout: 6000,
+    timeout: 8000,
     headers: {
         'Content-Type': 'application/json',
     },
